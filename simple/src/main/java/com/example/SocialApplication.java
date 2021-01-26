a42592e05fbd8f0dbd9287aaf70112f3b722331f
vie02 <trankhactaia1@gmail.com>
2021-01-26 14:37:14 +0700

    Finish tutorial from social-simple

--- simple/src/main/java/com/example/SocialApplication.java
+++ simple/src/main/java/com/example/SocialApplication.java
@@ -17,12 +17,123 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import static org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
-public class SocialApplication {
+@RestController
+public class SocialApplication extends WebSecurityConfigurerAdapter {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SocialApplication.class, args);
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("login"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        SimpleUrlAuthenticationFailureHandler handler = new SimpleUrlAuthenticationFailureHandler("/");
+
+        http
+                .authorizeRequests(a -> a
+                        .antMatchers("/", "/error", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .logout(l -> l
+                        .logoutSuccessUrl("/").permitAll()
+                )
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()) // send token to FE as cookie
+                )
+                .oauth2Login(o -> o //handle authentication exception
+                        .failureHandler((request, response, exception) -> {
+                            request.getSession().setAttribute("error.message", exception.getMessage());
+                            handler.onAuthenticationFailure(request, response, exception);
+                        })
+                );
+    }
+
+    @GetMapping("/error")
+    @ResponseBody
+    public String error(HttpServletRequest request) {
+        String message = (String) request.getSession().getAttribute("error.message");
+        request.getSession().removeAttribute("error.message");
+        return message;
+    }
+
+    @Bean
+    public WebClient rest(ClientRegistrationRepository clients, OAuth2AuthorizedClientRepository authz) {
+        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 =
+                new ServletOAuth2AuthorizedClientExchangeFilterFunction(clients, authz);
+        return WebClient.builder()
+                        .filter(oauth2).build();
+    }
+
+    /**
+	 * To spice things up a bit, you can extend the authentication rule to reject users that are not in the right organization.
+	 * You can use the GitHub API to find out more about the user, so you’ll just need to plug that into the right part of the authentication process.
+	 * Fortunately, for such a simple use case, Spring Boot has provided an easy extension point:
+	 * If you declare a @Bean of type OAuth2UserService, it will be used to identify the user principal.
+	 * You can use that hook to assert the the user is in the correct organization, and throw an exception if not:
+	 * */
+	@Bean
+	public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserOAuth2UserService(WebClient rest) {
+        DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+        return request -> {
+            OAuth2User user = delegate.loadUser(request);
+            if (!"github".equals(request.getClientRegistration().getRegistrationId())) {
+                return user;
+            }
+
+            OAuth2AuthorizedClient client = new OAuth2AuthorizedClient
+                    (request.getClientRegistration(), user.getName(), request.getAccessToken());
+            String url = user.getAttribute("organizations_url");
+            List<Map<String, Object>> orgs = rest
+                    .get().uri(url)
+                    .attributes(oauth2AuthorizedClient(client))
+                    .retrieve()
+                    .bodyToMono(List.class)
+                    .block();
+
+            if (orgs.stream().anyMatch(org -> "spring.projects".equals(org.get("login")))) {
+                return user;
+            }
+
+            throw new OAuth2AuthenticationException(new OAuth2Error("invalid_token", "Not in Spring Team", ""));
+        };
 	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialApplication.class, args);
+    }
 
 }
